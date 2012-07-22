330db70843eb79bf49f44c2ce614abe6ad1347bd
VoQn <VoQn.tyrantist@gmail.com>
2012-07-22 21:42:22 +0900

    Change :: coding style

--- src/function.js
+++ src/function.js
@@ -1,91 +1,73 @@
-'use strict';
+;(function(root) {
+  'use strict';
+  var function_util = {};
 
-if (require) {
-  var O = require('./object'),
-      asArray = O.asArray;
-}
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = function_util;
+  } else {
+    root.function_util = function_util;
+  }
 
-/**
- * @param {*} any value.
- * @return {*} any value.
- * @nosideeffects
- */
-function id(any) {
-  return any;
-}
+  var __slice = Array.prototype.slice;
 
-/**
- * @param {*} any value.
- * @param {*} second argument.
- * @return {*} 2nd argument.
- * @nosideeffects
- */
-function seq(any, second) {
-  return second;
-}
+  /**
+   * @param {*} any value.
+   * @return {*} any value.
+   * @nosideeffects
+   */
+  function_util.id = function id(any) { return any; };
 
-/**
- * @param {function():*} promise procedure.
- * @return {*} promise return value by promise.
- */
-function force(promise) {
-  return promise();
-}
+  /**
+   * @param {*} any value.
+   * @param {*} second argument.
+   * @return {*} 2nd argument.
+   * @nosideeffects
+   */
+  function_util.seq = function seq(any, second) { return second; };
 
-/**
- * @param {*} arg register for next function.
- * @param {...*} var_args for next function.
- * @return {function(function(...*):*):*} waiting callback with args.
- */
-function apply(arg, var_args) {
-  var args = asArray(arguments);
-  function applies(func) {
+  /**
+   * @param {function():*} promise procedure.
+   * @return {*} promise return value by promise.
+   */
+  function_util.force = function force(promise) { return promise(); };
+
+  /**
+   * @param {*} arg register for next function.
+   * @param {...*} var_args for next function.
+   * @return {function(function(...*):*):*} waiting callback with args.
+   */
+  function_util.apply = function apply(arg, var_args) {
+    var args = __slice.call(arguments);
+    return (args.length < 2) ?
+      function(func) {
+        return func(arg);
+      } :
+      function(func) {
+        return func.apply(null, args);
+      };
+  };
+
+  /**
+   * @param {function(...*):*} func to curry.
+   * @param {...*} var_args for callback.
+   * @return {function(...*):*|*}
+   * when var_args.length == func.length, return func(var_args),
+   * otherwise curried function.
+   */
+  function_util.curry = function curry(func, var_args) {
+    var require_length = func.length,
+        args = arguments.length > 1 ? __slice.call(arguments, 1) : [],
+        curried = function() {
+          var args2 = __slice.call(arguments),
+          appended = args.concat(args2);
+          return curry.apply(null, [func].concat(appended));
+        };
+
+    if (require_length > args.length) {
+      return curried;
+    }
     return func.apply(null, args);
-  }
-  function apply_one(func) {
-    return func(arg);
-  }
-  if (args.length === 1) {
-    return apply_one;
-  }
-  return applies;
-}
+  };
 
-/**
- * @param {function(...*):*} func to curry.
- * @param {...*} var_args for callback.
- * @return {function(...*):*|*}
- * when var_args.length == func.length, return func(var_args),
- * otherwise curried function.
- */
-function curry(func, var_args) {
-  var l = func.length,
-      args = arguments.length < 2 ? [] : asArray(arguments, 1);
-  function curried() {
-    var args2 = asArray(arguments),
-        appended = args.concat(args2);
-    return curry.apply(null, [func].concat(appended));
-  }
-  if (l > args.length) {
-    return curried;
-  }
-  return func.apply(null, args);
-}
-
-if (typeof exports !== 'undefined') {
-  /** @type {function(*):*} */
-  exports.id = id;
-
-  /** @type {function(*,*):*} */
-  exports.seq = seq;
-
-  /** @type {function(function():*):*} */
-  exports.force = force;
-
-  /** @type {function(*, ...*):(function(...*):*)} */
-  exports.apply = apply;
-
-  /** @type {function(function(...*), ...*):*|function(...*):*} */
-  exports.curry = curry;
-}
+})(this);
 // EOF
